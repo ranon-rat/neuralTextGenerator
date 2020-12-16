f2ed4190287bf6a9c0cf1ea6ae72b9ed10d82478
Glitch (hello-webpage) <none>
2020-12-16 04:28:28 +0000

    🍂👑 Checkpoint ./script.js:22284543/4310 ./index.html:22284543/409

--- script.js
+++ script.js
@@ -2,9 +2,40 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
+let options = {
+  inputs: ["x", "y"],
+  outputs: ["label"],
+  task: "classification",
+  debug: true
+};
+var model = ml5.neuralNetwork(options);
+
+const data = () => {
+  
+  var thisData = document.querySelector("#texto").outerText.split("");
+  
+  for (let i = 0; i <= thisData.length; i++) {
+    let options = {
+      x: i,
+      y: Math.floor(i / 60)
+    };
+    let dataOfThis = {
+      label: thisData[i]
+    };
+    model.addData(options, dataOfThis);
+  }
+};
+const trainModel = ()=>{
+  let options = {
+        epochs: 200
+      };
+      model.normalizeData();
+      model.train(options, whileTraining, finishTraining);
+}
+const whileTraining=()=>{}
+
 //
 /*
-document.querySelector("#texto").outerText es donde se encuentra el input
+document.querySelector("#texto").outerText.split("\n")
 el maximo tamaño que deberia de tener es | 60 |
 */
