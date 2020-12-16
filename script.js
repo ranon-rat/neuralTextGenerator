ef33ebdd44f6d43b6ab07d893f1b2d18b77c43bb
Glitch (hello-webpage) <none>
2020-12-16 04:16:50 +0000

    🌅👵 Checkpoint ./index.html:22284543/4728 ./style.css:22284543/321 ./script.js:22284543/312

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+//
+/*
+document.querySelector("#texto").outerText es donde se encuentra el input
+el maximo tamaño que deberia de tener es | 60 |
+*/
